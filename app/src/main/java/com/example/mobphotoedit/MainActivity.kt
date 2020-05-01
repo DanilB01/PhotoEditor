8469534da4e50bf82949ecc7bc9fac4434c98357
greyd <sashatuzikov2001@mail.ru>
2020-05-01 22:01:23 +0700

    Splash Screen

--- app/src/main/java/com/example/mobphotoedit/MainActivity.kt
+++ app/src/main/java/com/example/mobphotoedit/MainActivity.kt
@@ -2,6 +2,12 @@ package com.example.mobphotoedit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
 
 class MainActivity : AppCompatActivity() {
 
