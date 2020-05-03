2f3c584e235a8e751658ecddce39b3a679f503e0
SPQR <danil.b0101@bk.ru>
2020-05-03 18:04:47 +0700

    Menu's Design

--- app/src/main/java/com/example/mobphotoedit/DesktopActivity.kt
+++ app/src/main/java/com/example/mobphotoedit/DesktopActivity.kt
@@ -1,8 +1,10 @@
 package com.example.mobphotoedit
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.core.net.toUri
+import kotlinx.android.synthetic.main.activity_desktop.*
 
 
 class DesktopActivity : AppCompatActivity() {
@@ -12,7 +14,11 @@ class DesktopActivity : AppCompatActivity() {
         setContentView(R.layout.activity_desktop)
 
 
+        var string: String? = intent.getStringExtra("ImageUri")
+        var imageUri: Uri? = string?.toUri()
 
+
+        photo.setImageURI(imageUri)
     }
 
 }
--- app/src/main/java/com/example/mobphotoedit/MainActivity.kt
+++ app/src/main/java/com/example/mobphotoedit/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : AppCompatActivity() {
        private val IMAGE_PICK_CODE = 1001
        private val PERMISSION_CODE = 1000
    }
-    var imageUrii: Uri? = null
+    var imageUri: Uri? = null
     var flagCamera: Boolean = false
     var flagGallery: Boolean = false
 
@@ -36,13 +36,14 @@ class MainActivity : AppCompatActivity() {
 
         yes.setOnClickListener {
             val intent = Intent(MainActivity@this, DesktopActivity::class.java)
+            intent.putExtra("ImageUri", imageUri.toString())
             startActivity(intent)
         }
         no.setOnClickListener {
-            imageView.setImageResource(R.drawable.ic_image_black_24dp)
+            //imageView.setImageResource(R.drawable.ic_image_black_24dp)
             yes.visibility = View.GONE
             no.visibility = View.GONE
-            select.visibility = View.VISIBLE
+            message.text = getString(R.string.add)
         }
 
         takePhotoButton.setOnClickListener{
@@ -93,10 +94,10 @@ class MainActivity : AppCompatActivity() {
         val values = ContentValues()
         values.put(MediaStore.Images.Media.TITLE, "New Picture")
         values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
-        imageUrii = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
 
         val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUrii)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
         startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
     }
 
@@ -135,16 +136,18 @@ class MainActivity : AppCompatActivity() {
         if(resultCode == Activity.RESULT_OK){
             //показать полученное изображение в Image View
             if(requestCode == IMAGE_CAPTURE_CODE){
-                imageView.setImageURI(imageUrii)
+                //imageView.setImageURI(imageUri)
             }
             else {
                 if(requestCode == IMAGE_PICK_CODE){
-                    imageView.setImageURI(data?.data)
+                    imageUri = data?.data
+                    //imageView.setImageURI(imageUri)
                 }
             }
+
             yes.visibility = View.VISIBLE
             no.visibility = View.VISIBLE
-            select.visibility = View.GONE
+            message.text = getString(R.string.cont)
         }
     }
 
